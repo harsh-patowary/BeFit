e98af94359a8e833eb1ec7deb2c90e906098f4cc
Rick <harshpatowary@gmail.com>
2023-11-19 06:03:48 +0000

    calorie-fetching done with api

--- app/befit_utils.py
+++ app/befit_utils.py
@@ -1,5 +1,6 @@
 # Util Contents
-
+import os
+import csv
 class UserData:
     
     _age = None
@@ -8,6 +9,7 @@ class UserData:
     _name = None
     _bmi = None
     _unit = None
+    _calories_intake = None
     
     def __init__(self, a, w, h, n, u):
         self._age = a
@@ -42,12 +44,45 @@ class UserData:
                return 'very overweight'
         else:
             return 'Invalid Range Error:BMI/RA23'
+        
+        
+    def set_food_intake(self, food):
+        self._calories_intake = food
+        
+    def convert_to_array(self):
+        return [self._name, self._age, self._height, self._weight, self._unit, round(self._bmi), self._calories_intake]
+      
+      
        
-
+class BFUtils:
+    
+    def remove_space(ingr):
+        newStr = ""
+        for ch in ingr:
+            if ch == " ":
+                ch = "%20"
+            newStr+=ch
+        return newStr
+    
+    def insert_data(file_path, data):
+        file_exists = os.path.isfile("data.csv")
+        with open("data.csv", "a", newline='') as file:
+            csvwriter = csv.writer(file)
+                
+            if not file_exists:
+                header = ['Name', 'Age', 'height', 'weight', 'unit', 'bmi', 'calories']
+                csvwriter.writerow(header)
+                    
+            csvwriter.writerow(data)
+    
+    
 # if __name__ == "__main__":
 #     user1_utils = Utils(45, 60, 1.64, 'rick')
 #     us1_bmi = user1_utils.calc_bmi('m')
 #     print(f"bmi: {us1_bmi}")
 #     us1_status = user1_utils.bmi_class()
 #     print(f"status: {us1_status}")
-    
+    # ingredient = "mashed potatoes"
+#     newString = BFUtils.remove_space(ingredient)
+#     print(ingredient)
+#     print(newString)
--- app/main.py
+++ app/main.py
@@ -5,10 +5,15 @@ import json
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 # custom mods
-from app.befit_utils import UserData
+from app.befit_utils import UserData, BFUtils
+import os
+import csv
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'harsh004'
+app_id = "d16a2064"
+app_key = "754bc0f60796c0031d1da99d58a3f1da"
+
 
 @app.route('/')
 def index():
@@ -32,8 +37,26 @@ def bmi():
             usr_data = UserData(usr_age, usr_weight, usr_height, usr_name, bmi_unit)
             usr_bmiStatus = usr_data.bmi_class()
             usr_bmi = round(usr_data._bmi)
+            BFUtils.insert_data("/data.csv", usr_data.convert_to_array())
             return render_template('bmi.html', usr_bmi=usr_bmi, usr_bmiStatus=usr_bmiStatus)
     
     return render_template('bmi.html')
 
 
+@app.route('/calorietracker', methods = ['GET', 'POST'])
+def calorie_tracker():
+    if request.method == 'POST':
+        ingredient = BFUtils.remove_space(request.form['ingredient'])
+        if not ingredient:
+            flash("Ingrediant is needed")
+        else:
+            api_url = f"https://api.edamam.com/api/nutrition-data?app_id={app_id}&app_key={app_key}&nutrition-type=logging&ingr={ingredient}"
+            response = requests.get(api_url).json()
+            calories = response["calories"]
+            print(type(response))
+            print(f"Ingredients: {ingredient}, Calories: {calories}")
+            
+            render_template('calorie-tracker.html', ingredient=ingredient, calories=calories)
+            
+
+    return render_template('calorie-tracker.html')
